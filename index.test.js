d4b4fc65f9ed3f773ee8959baac4f2ae2a04f66e
Marta <martszcz@gmail.com>
2018-06-08 15:04:16 +0200

    returns score for 1 result

--- index.test.js
+++ index.test.js
@@ -1,5 +1,14 @@
-const score = results => "";
+const score = results => {
+  if (results.length === 0) {
+    return "";
+  }
+  return "a: 1";
+};
 
 test("returns empty string for empty string", () => {
   expect(score("")).toEqual("");
+});
+
+test("returns score for 1 result", () => {
+  expect(score("a")).toEqual("a: 1");
 });
